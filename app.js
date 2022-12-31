8555f5ad425ef137850ff1a5154d0ea180553bb6
Hamdi Ismail <hamdihismail@gmail.com>
2022-12-31 14:54:22 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const url =  "https://api.adviceslip.com/advice";
+const result = document.querySelector('.advice');
+const adviceId = document.querySelector('.advice-id');
+const btn = document.querySelector('.btn');
+
+const fetchAdvice = async () => {
+    result.textContent = 'Loading...';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error();
+      }
+      const data = await response.json();
+      const advice = data.slip.advice;
+      const id = data.slip.id;
+      result.textContent = advice;
+      adviceId.textContent = `Advice # ${id}`
+    } catch (error) {
+      result.textContent = 'There was an error...';
+    }
+  };
+
+btn.addEventListener('click', fetchAdvice)
+
+  fetchAdvice()
+
+  // const advice = async () => {
+  //   const resp = await fetch(url)
+  //   const data = await resp.json();
+  //   console.log(data.slip.advice);
+  // }
+  // advice()
